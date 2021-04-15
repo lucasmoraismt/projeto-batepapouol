d1b6873b7ac8e440d3c854f86d8beb5e2462eece
Lucas de Morais <lucasmoraismt@gmail.com>
2021-04-14 22:58:45 -0300

    Enviando mensagens com enter e atualizando o chat.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,28 @@
 
 let chat = document.querySelector(".chat");
-let userList = document.querySelector(".users");
+let userList = document.querySelector(".users .nicklist");
 let selectedUser = document.querySelector(".user.selected");
 let selectedPrivacy = document.querySelector(".privacy.selected");
-let name = document.querySelector(".user.selected .name");
+let name = document.querySelector(".user.selected .name").innerHTML;
 let privacy = document.querySelector(".privacy.selected span");
+let input = document.querySelector(".input input");
+let privacyType = 'message';
 let nick;
 
-//getUsers();
 getMessages();
-//login();
+//updateChat();
+login();
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("send").click();
+  }
+});
 
 function login() {
     nick = prompt("Qual seu nome?");
@@ -34,6 +47,12 @@ function usernameNotAvailable(error) {
 }
 
 function getUsers() {
+    let promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants');
+
+    promise.then(populateUsers);
+}
+
+function populateUsers() {
 
 }
 
@@ -41,59 +60,80 @@ function getMessages() {
     let promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
 
     promise.then(populateMessages);
+    chat.innerHTML = '';
 }
 
 function populateMessages(msgList) {
 
+    let messageHTML;
+    let currentMsg;
+
     for(i = 0; i < msgList.data.length; i++) {
+
         if(msgList.data[i].type === 'status') {
 
-            let currentMsg = msgList.data[i]
-            let messageHTML = `
+            currentMsg = msgList.data[i]
+            messageHTML = `
                 <div class="message status">
-                    <span class="time">(${currentMsg.time})</span>
-                    <span class="user">${currentMsg.from}</span>
-                    <span>${currentMsg.text}</span>
+                    <p>
+                        <span class="time">(${currentMsg.time})</span>
+                        <span class="user">${currentMsg.from}</span>
+                        <span>${currentMsg.text}</span>
+                    </p>
                 </div>`;
 
             chat.innerHTML += messageHTML;
 
         } else if(msgList.data[i].type === 'message') {
 
-            let currentMsg = msgList.data[i];
-            let messageHTML = `
+            currentMsg = msgList.data[i];
+            messageHTML = `
                 <div class="message">
-                    <span class="time">(${currentMsg.time})</span>
-                    <span class="user">${currentMsg.from}</span>
-                    <span>para</span>
-                    <span class="user">${currentMsg.to}</span>
-                    <span>: ${currentMsg.text}</span>
+                    <p>
+                        <span class="time">(${currentMsg.time})</span>
+                        <span class="user">${currentMsg.from}</span>
+                        <span>para</span>
+                        <span class="user">${currentMsg.to}</span>
+                        <span>: ${currentMsg.text}</span>
+                    </p>
                 </div>`;
 
             chat.innerHTML += messageHTML;
 
         } else if((msgList.data[i].to === nick && msgList.data[i].type === 'private_message') || (msgList.data[i].from === nick && msgList.data[i].type === 'private_message')){
 
-            let currentMsg = msgList.data[i];
-            let messageHTML = `
+            currentMsg = msgList.data[i];
+            messageHTML = `
                 <div class="message private">
-                    <span class="time">(${currentMsg.time})</span>
-                    <span class="user">${currentMsg.from}</span>
-                    <span>privado para</span>
-                    <span class="user">${currentMsg.to}</span>
-                    <span>${currentMsg.text}</span>
+                    <p>
+                        <span class="time">(${currentMsg.time})</span>
+                        <span class="user">${currentMsg.from}</span>
+                        <span>privado para</span>
+                        <span class="user">${currentMsg.to}</span>
+                        <span>${currentMsg.text}</span>
+                    </p>
                 </div>`;
 
             chat.innerHTML += messageHTML;
         }
     }
+
+    chat.scrollTop = chat.scrollHeight;
 }
 
 function sendMessage(input) {
+    let typed = document.getElementById("typing").value;
+    let newMessage = {from: `${nick}`, to: name, text: `${typed}`, type: `${privacyType}`};
 
+    let promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages', newMessage);
+
+    promise.then(getMessages);
+    typed = '';
 }
 
 function openUsers() {
+    // getUsers();
+
     let sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "block";
 }
@@ -107,7 +147,7 @@ function selectUser(user) {
     selectedUser.classList.remove("selected");
     user.classList.add("selected");
     selectedUser = user;
-    name = document.querySelector(".user.selected .name");
+    name = document.querySelector(".user.selected .name").innerHTML;
     updateSending();
 }
 
@@ -116,37 +156,26 @@ function selectPrivacy(pvt) {
     pvt.classList.add("selected");
     selectedPrivacy = pvt;
     privacy = document.querySelector(".privacy.selected span");
+    
+    if(privacy.innerHTML === "Privado") {
+        privacyType = 'private_message';
+    } else {
+        privacyType = 'message';
+    }
     updateSending();
+}
+
+function updateChat() {
+
+    setTimeout(getMessages, 3000);
+
 }
 
 function updateSending() {
     let sending = document.querySelector(".sending");
-    let user = name.innerHTML;
+    let user = name;
     let pvt = privacy.innerHTML;
     sending.innerHTML = `Enviando para ${user} (${pvt})`;
 }
 
-{/* <div class="message status">
-        <span class="time">(12:29:22)</span>
-        <span class="user">João</span>
-        <span>entrou.</span>
-    </div>
-    <div class="message private">
-        <span class="time">(12:29:33)</span>
-        <span class="user">Sálvio</span>
-        <span>privado para</span>
-        <span class="user">Marilene:</span>
-        <span>Olá, Marilene!</span>
-    </div>
-    <div class="message">
-        <span class="time">(12:30:02)</span>
-        <span class="user">Ana</span>
-        <span>para</span>
-        <span class="user">Todos:</span>
-        <span>Tudo bem, pessoal?</span>
-    </div>
-    <div class="message status">
-        <span class="time">(12:30:45)</span>
-        <span class="user">Marilene</span>
-        <span>saiu.</span>
-    </div> */}
+// https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants
